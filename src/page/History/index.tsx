815d254cb79ce339dad458943f2dc0d9688eaa62
Mauricio Girardi <maurigirarde@yahoo.com.br>
2022-07-19 17:43:04 -0300

    Create table

---
+++ src/page/History/index.tsx
@@ -0,0 +1,89 @@
+import * as S from './styles'
+
+export const History = () => {
+  return (
+    <S.HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <S.HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 mínutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <S.Status statusColor="green">Concluído</S.Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 mínutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <S.Status statusColor="green">Concluído</S.Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 mínutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <S.Status statusColor="green">Concluído</S.Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 mínutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <S.Status statusColor="green">Concluído</S.Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 mínutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <S.Status statusColor="green">Concluído</S.Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 mínutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <S.Status statusColor="yellow">Em andamento</S.Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 mínutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <S.Status statusColor="green">Concluído</S.Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 mínutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <S.Status statusColor="red">Interrompido</S.Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </S.HistoryList>
+    </S.HistoryContainer>
+  )
+}
